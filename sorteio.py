dad0d5c4518e00d30bc7b417e12586709c7c5323
thiagomath <thiagocruz.mat@gmail.com>
2021-09-05 13:26:15 -0300

    Primeira janela PySimpleGUI

--- sorteio.py
+++ sorteio.py
@@ -1,3 +1,20 @@
-import pysimplegui as sg
+import PySimpleGUI as sg
 
-class
+#Layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário:'), sg.Input(key='Usuário')],
+    [sg.Text('Senha:'), sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+#Janela
+janela = sg.Window('Tela de Login', layout)
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['Usuário'] == 'Thiago' and valores['senha'] == '123456':
+            print('Bem vindo à Thiago.DEV')
